48530b3868f5d0be29d4db3ada10f1ab678f86dc
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-02 05:26:48 +0300

    final

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,22 +9,22 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 if __name__ == "__main__":
-   user_id = sys.argv[1]
-   url = "https://jsonplaceholder.typicode.com/"
-   user = requests.get(url + "users/{}".format(user_id), verify=False).json()
-   username = user.get("username")
-   todos = requests.get(url + "todos", params={
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id), verify=False).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={
        "userId": user_id}, verify=False).json()
 
-   tasks = []
-   for t in todos:
-       task = {}
-       task["task"] = t.get("title")
-       task["completed"] = t.get("completed")
-       task["username"] = username
-       tasks.append(task)
+    tasks = []
+    for t in todos:
+        task = {}
+        task["task"] = t.get("title")
+        task["completed"] = t.get("completed")
+        task["username"] = username
+        tasks.append(task)
 
-   data = {user_id: tasks}
+    data = {user_id: tasks}
 
-   with open("{}.json".format(user_id), "w") as jsonfile:
-       json.dump(data, jsonfile, indent=4)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump(data, jsonfile, indent=4)
