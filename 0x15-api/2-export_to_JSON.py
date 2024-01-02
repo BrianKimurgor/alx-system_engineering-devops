514430faa89dea4520f81c2090a3970fb8135006
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-02 05:22:46 +0300

    pycodestyle

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-import csv
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
 import requests
 import sys
 import urllib3
@@ -9,17 +9,22 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id), verify=False).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={
-        "userId": user_id}, verify=False).json()
+   user_id = sys.argv[1]
+   url = "https://jsonplaceholder.typicode.com/"
+   user = requests.get(url + "users/{}".format(user_id), verify=False).json()
+   username = user.get("username")
+   todos = requests.get(url + "todos", params={
+       "userId": user_id}, verify=False).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [
-            writer.writerow([user_id, username, t.get(
-                "completed"), t.get("title")])
-            for t in todos
-        ]
+   tasks = []
+   for t in todos:
+       task = {}
+       task["task"] = t.get("title")
+       task["completed"] = t.get("completed")
+       task["username"] = username
+       tasks.append(task)
+
+   data = {user_id: tasks}
+
+   with open("{}.json".format(user_id), "w") as jsonfile:
+       json.dump(data, jsonfile, indent=4)
